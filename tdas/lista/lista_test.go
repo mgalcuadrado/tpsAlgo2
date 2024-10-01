9d00c6012369284f7cfd0a6e4eaa0668fceebefa
ahauserman <ahauserman@fi.uba.ar>
2024-10-01 12:04:59 -0300

    mas pruebas de iterador interno

--- tdas/lista/lista_test.go
+++ tdas/lista/lista_test.go
@@ -1,9 +1,11 @@
 package lista_test
 
 import (
-	"github.com/stretchr/testify/require"
+	"fmt"
 	TDALista "tdas/lista"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -29,9 +31,9 @@ func TestInsertarUnElementoPrimero(t *testing.T) {
 	require.Equal(t, 1, lista.Largo())
 }
 
-func TestInsertarDiezElementosPrimero(t * testing.T) {
+func TestInsertarDiezElementosPrimero(t *testing.T) {
 	lista := TDALista.CrearListaEnlazada[int]()
-	for i:= 1; i <= 10; i++ {
+	for i := 1; i <= 10; i++ {
 		lista.InsertarPrimero(i * 2)
 		require.False(t, lista.EstaVacia())
 		require.Equal(t, i, lista.Largo())
@@ -46,9 +48,9 @@ func TestInsertarUnElementoUltimo(t *testing.T) {
 	require.Equal(t, 1, lista.Largo())
 }
 
-func TestInsertarDiezElementosUltimo(t * testing.T) {
+func TestInsertarDiezElementosUltimo(t *testing.T) {
 	lista := TDALista.CrearListaEnlazada[int]()
-	for i:= 1; i <= 10; i++ {
+	for i := 1; i <= 10; i++ {
 		lista.InsertarUltimo(i * 2)
 		require.False(t, lista.EstaVacia())
 		require.Equal(t, i, lista.Largo())
@@ -65,71 +67,71 @@ func TestBorrarUnElemento(t *testing.T) {
 	require.True(t, lista.EstaVacia())
 }
 
-func TestInsertarPrimeroYBorrarDiezElementos(t * testing.T) {
+func TestInsertarPrimeroYBorrarDiezElementos(t *testing.T) {
 	lista := TDALista.CrearListaEnlazada[int]()
-	for i:= 1; i <= 10; i++ {
+	for i := 1; i <= 10; i++ {
 		lista.InsertarPrimero(i * 2)
 		require.False(t, lista.EstaVacia())
 		require.Equal(t, i, lista.Largo())
 	}
-	for i:= 10; i >= 1; i-- {
+	for i := 10; i >= 1; i-- {
 		require.False(t, lista.EstaVacia())
-		require.Equal(t, i * 2, lista.BorrarPrimero())
+		require.Equal(t, i*2, lista.BorrarPrimero())
 	}
 	require.True(t, lista.EstaVacia())
 }
 
-func TestInsertarUltimoYBorrarDiezElementos(t * testing.T) {
+func TestInsertarUltimoYBorrarDiezElementos(t *testing.T) {
 	lista := TDALista.CrearListaEnlazada[int]()
-	for i:= 1; i <= 10; i++ {
+	for i := 1; i <= 10; i++ {
 		lista.InsertarUltimo(i * 2)
 		require.False(t, lista.EstaVacia())
 		require.Equal(t, i, lista.Largo())
 	}
-	for i:= 1; i <= 10; i++ {
+	for i := 1; i <= 10; i++ {
 		require.False(t, lista.EstaVacia())
-		require.Equal(t, i * 2, lista.BorrarPrimero())
+		require.Equal(t, i*2, lista.BorrarPrimero())
 	}
 	require.True(t, lista.EstaVacia())
 }
 
 /* **************** Mixeando InsertarPrimero() e InsertarUltimo() **************** */
 
-func TestInsertarMixeadoDiezElementos(t * testing.T) {
+func TestInsertarMixeadoDiezElementos(t *testing.T) {
 	lista := TDALista.CrearListaEnlazada[int]()
-	for i:= 1; i <= 3; i++ {
+	for i := 1; i <= 3; i++ {
 		lista.InsertarPrimero(i * 2)
 		require.False(t, lista.EstaVacia())
 		require.Equal(t, i, lista.Largo())
 	} // [3 2 1]
-	for i:= 4; i <= 7; i++ {
+	for i := 4; i <= 7; i++ {
 		lista.InsertarUltimo(i * 2)
 		require.False(t, lista.EstaVacia())
 		require.Equal(t, i, lista.Largo())
 	} // [3 2 1 4 5 6 7]
-	for i:=8; i <= 10; i++ {
+	for i := 8; i <= 10; i++ {
 		lista.InsertarPrimero(i * 2)
 		require.False(t, lista.EstaVacia())
 		require.Equal(t, i, lista.Largo())
 	} //[10 9 8 3 2 1 4 5 6 7]
-	for i:= 10; i >= 8; i-- {
+	for i := 10; i >= 8; i-- {
 		require.False(t, lista.EstaVacia())
-		require.Equal(t, i * 2, lista.BorrarPrimero())
+		require.Equal(t, i*2, lista.BorrarPrimero())
 	} //[3 2 1 4 5 6 7]
 	require.Equal(t, 7, lista.Largo())
-	for i:= 3; i >= 1; i-- {
+	for i := 3; i >= 1; i-- {
 		require.False(t, lista.EstaVacia())
-		require.Equal(t, i * 2, lista.BorrarPrimero())
-	}//[4 5 6 7]
+		require.Equal(t, i*2, lista.BorrarPrimero())
+	} //[4 5 6 7]
 	require.Equal(t, 4, lista.Largo())
-	for i:= 4; i <= 7; i++ {
+	for i := 4; i <= 7; i++ {
 		require.False(t, lista.EstaVacia())
-		require.Equal(t, i * 2, lista.BorrarPrimero())
+		require.Equal(t, i*2, lista.BorrarPrimero())
 	}
 	require.True(t, lista.EstaVacia())
 }
 
-//yo acá agregaría alguna prueba más 
+//yo acá agregaría alguna prueba más
 
 /* **************** VerPrimero() y VerUltimo() **************** */
 func TestVerPrimeroUnElemento(t *testing.T) {
@@ -153,7 +155,6 @@ func TestVerUltimoUnElementoInsertandoUltimo(t *testing.T) {
 	require.True(t, lista.EstaVacia())
 	require.PanicsWithValue(t, _MENSAJE_PANIC_LISTA_VACIA, func() { lista.VerUltimo() }, _MENSAJE_TESTING_PANIC_LISTA_VACIA)
 }
-
 
 func TestVerUltimoUnElementoInsertandoPrimero(t *testing.T) {
 	lista := TDALista.CrearListaEnlazada[int]()
@@ -181,58 +182,61 @@ func TestVerDosElementos(t *testing.T) {
 	require.True(t, lista.EstaVacia())
 }
 
-func TestVerMixeadoDiezElementos(t * testing.T) {
+func TestVerMixeadoDiezElementos(t *testing.T) {
 	lista := TDALista.CrearListaEnlazada[int]()
-	for i:= 1; i <= 3; i++ {
+	for i := 1; i <= 3; i++ {
 		lista.InsertarPrimero(i * 2)
-		require.Equal(t, i * 2, lista.VerPrimero())
+		require.Equal(t, i*2, lista.VerPrimero())
 		require.Equal(t, 2, lista.VerUltimo())
 		require.False(t, lista.EstaVacia())
 		require.Equal(t, i, lista.Largo())
 	} // [3 2 1]
-	for i:= 4; i <= 7; i++ {
+	for i := 4; i <= 7; i++ {
 		lista.InsertarUltimo(i * 2)
-		require.Equal(t, 3 * 2, lista.VerPrimero())
-		require.Equal(t, i * 2, lista.VerUltimo())
+		require.Equal(t, 3*2, lista.VerPrimero())
+		require.Equal(t, i*2, lista.VerUltimo())
 		require.False(t, lista.EstaVacia())
 		require.Equal(t, i, lista.Largo())
 	} // [3 2 1 4 5 6 7]
-	for i:=8; i <= 10; i++ {
+	for i := 8; i <= 10; i++ {
 		lista.InsertarPrimero(i * 2)
-		require.Equal(t, i * 2, lista.VerPrimero())
-		require.Equal(t, 7 * 2, lista.VerUltimo())
+		require.Equal(t, i*2, lista.VerPrimero())
+		require.Equal(t, 7*2, lista.VerUltimo())
 		require.False(t, lista.EstaVacia())
 		require.Equal(t, i, lista.Largo())
 	} //[10 9 8 3 2 1 4 5 6 7]
-	for i:= 10; i >= 8; i-- {
-		require.Equal(t, i * 2, lista.VerPrimero())
-		require.Equal(t, 7 * 2, lista.VerUltimo())
+	for i := 10; i >= 8; i-- {
+		require.Equal(t, i*2, lista.VerPrimero())
+		require.Equal(t, 7*2, lista.VerUltimo())
 		require.False(t, lista.EstaVacia())
-		require.Equal(t, i * 2, lista.BorrarPrimero())
+		require.Equal(t, i*2, lista.BorrarPrimero())
 	} //[3 2 1 4 5 6 7]
 	require.Equal(t, 7, lista.Largo())
-	for i:= 3; i >= 1; i-- {
-		require.Equal(t, i * 2, lista.VerPrimero())
-		require.Equal(t, 7 * 2, lista.VerUltimo())
+	for i := 3; i >= 1; i-- {
+		require.Equal(t, i*2, lista.VerPrimero())
+		require.Equal(t, 7*2, lista.VerUltimo())
 		require.False(t, lista.EstaVacia())
-		require.Equal(t, i * 2, lista.BorrarPrimero())
-	}//[4 5 6 7]
+		require.Equal(t, i*2, lista.BorrarPrimero())
+	} //[4 5 6 7]
 	require.Equal(t, 4, lista.Largo())
-	for i:= 4; i <= 7; i++ {
-		require.Equal(t, i * 2, lista.VerPrimero())
-		require.Equal(t, 7 * 2, lista.VerUltimo())
+	for i := 4; i <= 7; i++ {
+		require.Equal(t, i*2, lista.VerPrimero())
+		require.Equal(t, 7*2, lista.VerUltimo())
 		require.False(t, lista.EstaVacia())
-		require.Equal(t, i * 2, lista.BorrarPrimero())
+		require.Equal(t, i*2, lista.BorrarPrimero())
 	}
 	require.True(t, lista.EstaVacia())
 }
 
-func TestIterarInternoPocosElementos(t * testing.T) {
+func TestIterarInternoPocosElementos(t *testing.T) {
 	lista := TDALista.CrearListaEnlazada[int]()
-	for i:= 1; i <= 10; i++ {
+	for i := 1; i <= 10; i++ {
 		lista.InsertarPrimero(i)
+		require.Equal(t, i, lista.Largo())
 	}
-	lista.Iterar(func MostrarValor(v int) bool{
+	require.Equal(t, 10, lista.VerPrimero())
+	require.Equal(t, 1, lista.VerUltimo())
+	lista.Iterar(func(v int) bool {
 		fmt.Println(v)
 		return true
 	})
