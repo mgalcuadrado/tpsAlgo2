f7c73a4c26eb98f0323dfdce83681b5610b26a9d
mgalcuadrado <110743194+mgalcuadrado@users.noreply.github.com>
2024-10-18 18:49:31 -0300

    Correcciones realizadas del hash

--- tdas/diccionario/hash.go
+++ tdas/diccionario/hash.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package diccionario
 
 import (
@@ -195,7 +194,7 @@ func (hash *hashCerrado[K, V]) Iterar(visitar func(clave K, dato V) bool) {
 func (hash *hashCerrado[K, V]) Iterador() IterDiccionario[K, V] {
 	iterador := new(iterHashCerrado[K, V])
 	iterador.hash = hash
-	iterador.indiceDelSiguiente(true)
+	iterador.avanzarAlSiguienteOcupado()
 	return iterador
 }
 
@@ -213,254 +212,20 @@ func (iterador *iterHashCerrado[K, V]) VerActual() (K, V) {
 // Siguiente avanza el iterador a la siguiente posición ocupada del hash. Si se terminó la iteración da panic
 func (iterador *iterHashCerrado[K, V]) Siguiente() {
 	iterador.verificarSiguiente()
-	iterador.indiceDelSiguiente(false)
+	iterador.indice_actual++
+	iterador.avanzarAlSiguienteOcupado()
 }
 
+// verificarSiguiente() corrobora si hay un siguiente; si no lo hay, da un panic. 
 func (iterador *iterHashCerrado[K, V]) verificarSiguiente() {
 	if !iterador.HaySiguiente() {
 		panic(_MENSAJE_PANIC_FIN_DE_ITERACION)
 	}
 }
 
-// indiceDelSiguiente es una función interna que devuelve el índice en el que está el siguiente elemento válido.
-func (iterador *iterHashCerrado[K, V]) indiceDelSiguiente(inicio bool) {
-	if !inicio {
-		iterador.indice_actual++
-	}
+// avanzarAlSiguienteOcupado es una función interna que avanza el indice_actual del iterador a la siguiente posición en la que hay un elemento válido.
+func (iterador *iterHashCerrado[K, V]) avanzarAlSiguienteOcupado() {
 	for (iterador.indice_actual < iterador.hash.capacidadCeldas) && iterador.hash.celdas[iterador.indice_actual].estado != _OCUPADO {
 		iterador.indice_actual++
 	}
 }
-=======
-package diccionario
-
-import (
-	"fmt"
-)
-
-/* **************** DEFINICIÓN DE VARIABLES **************** */
-const (
-	_MENSAJE_PANIC_CLAVE_NO_PERTENECE_A_DICCIONARIO string = "La clave no pertenece al diccionario"
-	_MENSAJE_PANIC_FIN_DE_ITERACION                 string = "El iterador termino de iterar"
-
-	_FACTOR_DE_CARGA_MAXIMO                  float64 = 0.7 //esto lo podemos modificar
-	_FACTOR_DE_CARGA_MINIMO                  float64 = 0.2
-	_CANTIDAD_INICIAL                        int     = 53
-	_MULTIPLICADOR_DE_INCREMENTO_DE_CANTIDAD int     = 2
-	_MULTIPLICADOR_DE_DECREMENTO_DE_CANTIDAD float64 = 2 / 3
-)
-
-/* **************** DEFINICIÓN DE VARIABLES **************** */
-type estados int
-
-const (
-	_VACIO estados = iota //a mirar
-	_OCUPADO
-	_BORRADO
-)
-
-type celda[K comparable, V any] struct {
-	clave  K
-	valor  V
-	estado estados
-}
-
-type hashCerrado[K comparable, V any] struct {
-	celdas       []celda[K, V]
-	largo        int
-	cantElem     int
-	cantBorrados int
-}
-
-type IterHashCerrado[K comparable, V any] struct {
-	hash          *hashCerrado[K, V]
-	indice_actual int
-}
-
-/* **************** IMPLEMENTACIÓN DEL HASH **************** */
-
-/* **************** FUNCIONES DE LA INTERFACE **************** */
-func CrearHash[K comparable, V any]() Diccionario[K, V] {
-	return crearHash[K, V](_CANTIDAD_INICIAL)
-}
-
-func (hash *hashCerrado[K, V]) Guardar(clave K, dato V) {
-	factorDeCarga := float64(hash.cantElem+hash.cantBorrados) / float64(hash.largo)
-	if (factorDeCarga) >= _FACTOR_DE_CARGA_MAXIMO {
-		hash.redimensionarHash(int(hash.largo * _MULTIPLICADOR_DE_INCREMENTO_DE_CANTIDAD))
-	}
-	pertenece, posicion := hash.buscar(clave)
-	if pertenece {
-		hash.celdas[posicion].valor = dato
-	} else {
-		hash.celdas[posicion].estado = _OCUPADO
-		hash.celdas[posicion].clave = clave
-		hash.celdas[posicion].valor = dato
-		hash.cantElem++ //mantengo invariante de representación
-	}
-}
-
-func (hash *hashCerrado[K, V]) Pertenece(clave K) bool {
-	pertenece, _ := hash.buscar(clave)
-	return pertenece
-}
-
-func (hash *hashCerrado[K, V]) Obtener(clave K) V {
-	posicion := hash.verificarPosicion(clave) //si llego al return es porque no saltó el panic
-	return hash.celdas[posicion].valor
-}
-
-func (hash *hashCerrado[K, V]) Borrar(clave K) V {
-	//if float64(hash.cantElem + hash.cantBorrados) /float64(hash.largo) <= _FACTOR_DE_CARGA_MINIMO {
-	//hash.redimensionarHash(int(float64(hash.largo) * _MULTIPLICADOR_DE_DECREMENTO_DE_CANTIDAD))
-	//}
-	posicion := hash.verificarPosicion(clave)
-	hash.cantBorrados++
-	hash.cantElem--
-	hash.celdas[posicion].estado = _BORRADO
-	return hash.celdas[posicion].valor
-}
-
-func (hash *hashCerrado[K, V]) Cantidad() int {
-	return hash.cantElem
-}
-
-/* **************** Funciones Auxiliares **************** */
-
-// crearHash es una función auxiliar que recibe el largo del que se quiere que sea la tabla del hash y devuelve un hashCerrado
-func crearHash[K comparable, V any](largo int) *hashCerrado[K, V] {
-	hash := new(hashCerrado[K, V])
-	hash.celdas = make([]celda[K, V], largo)
-	hash.largo = largo
-	return hash
-}
-
-// verificarPosicion recibe la clave y verifica si la posición es válida o no. Se usa al obtener y borrar, pues si no pertenece devuelve panic
-func (hash *hashCerrado[K, V]) verificarPosicion(clave K) int {
-	if hash.Cantidad() == 0 {
-		panic(_MENSAJE_PANIC_CLAVE_NO_PERTENECE_A_DICCIONARIO)
-	}
-	pertenece, posicion := hash.buscar(clave)
-	if !pertenece {
-		panic(_MENSAJE_PANIC_CLAVE_NO_PERTENECE_A_DICCIONARIO)
-	}
-	return posicion
-}
-
-// redimensionarHash recibe el nuevoLargo de la tabla de hashing y reemplaza el hash por el nuevo
-func (hash *hashCerrado[K, V]) redimensionarHash(nuevoLargo int) {
-	hashNuevo := crearHash[K, V](nuevoLargo)
-	for i := 0; i < hash.largo; i++ {
-		if hash.celdas[i].estado == _OCUPADO {
-			hashNuevo.Guardar(hash.celdas[i].clave, hash.celdas[i].valor)
-		}
-	}
-	//acá hash.cantElem == hashNuevo.cantElem siempre
-	hash.celdas = hashNuevo.celdas
-	hash.largo = hashNuevo.largo
-	hash.cantBorrados = hashNuevo.cantBorrados
-}
-
-// buscar recibe la clave y la busca en el arreglo de celdas. Devuelve un booleano indicando si la clave se halló en el arreglo y la posición en la que esa clave se encuentra en el arreglo.
-func (hash *hashCerrado[K, V]) buscar(clave K) (bool, int) {
-	indice := funcionDeHashing(clave, hash.largo)
-	if hash.celdas[indice].estado == _VACIO { //si no hay elementos en el hash o la celda en la que debería estar esa clave está vacía, entonces no está la clave
-		return false, indice
-	}
-	for hash.celdas[indice].estado != _VACIO {
-
-		if hash.celdas[indice].estado == _OCUPADO && hash.celdas[indice].clave == clave {
-			return true, indice
-		}
-		if indice == len(hash.celdas)-1 {
-			indice = 0
-		} else {
-			indice++
-		}
-	}
-	return false, indice
-}
-
-/* **************** Función de hashing **************** */
-
-// funcionDeHashing recibe la clave y devuelve un entero
-func funcionDeHashing[K comparable](clave K, largo int) int {
-	claveEnBytes := convertirABytes[K](clave)
-	return int(fnv1aHash(claveEnBytes)) % largo
-}
-
-func convertirABytes[K comparable](clave K) []byte {
-	return []byte(fmt.Sprintf("%v", clave))
-}
-
-func fnv1aHash(data []byte) uint32 {
-	const FNV_prime uint32 = 0x1000193
-	const offset_basis uint32 = 0x811C9DC5
-	// Iniciar el hash con el offset basis
-	hash := offset_basis
-	// Iterar sobre cada byte en el array de bytes
-	for _, b := range data {
-		// XOR el byte actual con el hash
-		hash ^= uint32(b)
-		// Multiplicar por el prime (FNV prime)
-		hash *= FNV_prime
-	}
-	return hash
-}
-
-/* **************** IMPLEMENTACIÓN DEL ITERADOR INTERNO **************** */
-
-/* **************** IMPLEMENTACIÓN DEL ITERADOR INTERNO **************** */
-
-func (hash *hashCerrado[K, V]) Iterar(visitar func(clave K, dato V) bool) {
-	for i := 0; i < hash.largo; i++ {
-		if hash.celdas[i].estado == _OCUPADO && !visitar(hash.celdas[i].clave, hash.celdas[i].valor) {
-			return
-		}
-	}
-}
-
-/* **************** IMPLEMENTACIÓN DEL ITERADOR EXTERNO **************** */
-
-func (hash *hashCerrado[K, V]) Iterador() IterDiccionario[K, V] {
-	iterador := new(IterHashCerrado[K, V])
-	iterador.hash = hash
-	iterador.indice_actual = iterador.indiceDelSiguiente(true)
-	return iterador
-}
-
-func (iterador *IterHashCerrado[K, V]) HaySiguiente() bool {
-	return iterador.indice_actual != iterador.hash.largo
-}
-
-func (iterador *IterHashCerrado[K, V]) VerActual() (K, V) {
-	if !iterador.HaySiguiente() {
-		panic(_MENSAJE_PANIC_FIN_DE_ITERACION)
-	}
-	return iterador.hash.celdas[iterador.indice_actual].clave, iterador.hash.celdas[iterador.indice_actual].valor
-}
-
-func (iterador *IterHashCerrado[K, V]) Siguiente() {
-	if !iterador.HaySiguiente() {
-		panic(_MENSAJE_PANIC_FIN_DE_ITERACION)
-	} else {
-		iterador.indice_actual = iterador.indiceDelSiguiente(false)
-	}
-}
-
-// esta función interna devuelve el índice en el que está el siguiente elemento válido. Si indice = hash.largo entonces no hay siguiente
-func (iterador *IterHashCerrado[K, V]) indiceDelSiguiente(inicio bool) int {
-	if iterador.indice_actual == iterador.hash.largo {
-		return iterador.indice_actual
-	}
-
-	indice := iterador.indice_actual
-	if !inicio {
-		indice++
-	}
-	for (indice < iterador.hash.largo) && iterador.hash.celdas[indice].estado != _OCUPADO {
-		indice++
-	}
-	return indice
-}
->>>>>>> refs/remotes/origin/main
